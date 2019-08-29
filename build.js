7eddd7565969a02de1c3952f310b000008f22d92
Mattia <matteron96@gmail.com>
2019-08-29 19:19:11 -0400

    Finished build system

--- build.js
+++ build.js
@@ -6,19 +6,20 @@ const fs = require('fs');
 const site = require('./site.json');
 
 const dirs = {
-	dist: './dist',
-	html: './pages',
-	css: './css',
-	js : './js',
-	fonts: './fonts'
+	dist: 'dist',
+	html: 'pages',
+	css: 'css',
+	js : 'js',
+	fonts: 'fonts'
 };
+const cssFileName = 'ugly';
 
 // CLEAN UP DIST
 
 rimraf.sync(dirs.dist);
 fs.mkdirSync(dirs.dist);
 
-console.log('Cleaned ' + dirs.dist);
+console.log('Cleaned ' + dirs.dist + '\n');
 
 // COMPILE MUSTACHE TEMPLATES DOWN TO MINIFIED HTML
 
@@ -41,11 +42,28 @@ site.forEach(page => {
 	fs.writeFileSync(dirs.dist + '/' + page.name + '.html', output);
 	console.log(' - Finished Compiling: ' + page.name);
 });
-console.log('Finished Compiling templates');
+console.log('Finished Compiling Templates\n');
+
+// UGLIFY CSS
 
 console.log('Uglifying CSS...');
-fs.readdir(dirs.css, (err, files) => {
-	if(err) {
-		console.log('Error')
-	}
+fs.mkdirSync(dirs.dist + '/' + dirs.css);
+var sheets = fs.readdirSync(dirs.css);
+sheets.forEach((path, i) => {
+	sheets[i] = dirs.css + '/' + path;
+})
+var css = uglifycss.processFiles(sheets);
+fs.writeFileSync(dirs.dist + '/' + dirs.css + '/' + cssFileName + '.css', css);
+console.log(' - Wrote to: ' + cssFileName + '.css');
+console.log('Finished Uglifying\n');
+
+// COPY FONTS OVER
+console.log('Copying Fonts...');
+fs.mkdirSync(dirs.dist + '/' + dirs.fonts);
+fs.readdirSync(dirs.fonts).forEach((font) => {
+	fs.copyFileSync(dirs.fonts + '/' + font, dirs.dist + '/' + dirs.fonts + '/' + font);
+	console.log(' - Copied: ' + font);
 });
+console.log('Finished Copying Fonts\n');
+
+console.log('All Done 👍');
