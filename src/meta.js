c8a9197ace80d5d89077fe68f43587b175dfef48
Mattia Schiano <mattia@Mattias-MacBook-Pro.local>
2020-05-22 00:11:45 -0400

    Preload only the neccesary fonts

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { normalizedJoin } = require('buildspace/lib/helpers');
+const { normalizedJoin, distinct } = require('buildspace/lib/helpers');
 
 const inputDir = 'src';
 
@@ -10,8 +10,25 @@ const accentColors = {
 	blue: 'blue'
 };
 
+const extOrder = [
+	'.woff2',
+	'.woff',
+	'.ttf',
+	'.svg',
+	'.eot'
+];
+
 const fonts = fs.readdirSync(normalizedJoin(inputDir, 'media', 'fonts'))
-	.map(f => normalizedJoin('/media', 'fonts', f));
+	.map(f => f.split('.')[0])
+	.filter(distinct)
+	.map(f => {
+		const ext = extOrder.find(ext => {
+			const path = normalizedJoin(inputDir, 'media', 'fonts', f + ext);
+			return fs.existsSync(path);
+		});
+		return f + ext;
+	})
+	.map(f => normalizedJoin('/media', 'fonts', f))
 
 module.exports = {
 	inputDir,
